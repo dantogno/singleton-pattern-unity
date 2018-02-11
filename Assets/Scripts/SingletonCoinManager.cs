a293c2a285403b7971209764c1247c056826ba98
dantogno <vebulidbih@msn.com>
2018-02-11 12:17:48 -0600

    initial project add

---
+++ Assets/Scripts/SingletonCoinManager.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SingletonCoinManager : MonoBehaviour
+{
+    private static SingletonCoinManager instance;
+
+    public static SingletonCoinManager Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                // instance = GameObject.Find("SingletonCoinManager").GetComponent<SingletonCoinManager>();
+                var gameObject = new GameObject(name: typeof(SingletonCoinManager).Name);
+                instance = gameObject.AddComponent<SingletonCoinManager>();
+                DontDestroyOnLoad(gameObject);
+            }
+
+            return instance;
+        }
+    }
+
+    public int CoinCount { get; set; }
+	
+}
